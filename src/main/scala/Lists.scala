f9281ad60384881866686f7fae1a0922090aa41c
Dan Luu <danluu@gmail.com>
2012-11-09 00:10:16 -0600

    tests and implementations for next 18 problems (up through 25)

--- src/main/scala/Lists.scala
+++ src/main/scala/Lists.scala
@@ -7,4 +7,85 @@ object Lists{
 	def length[A] (xs: List[A]) = xs.length
 	def reverse[A] (xs: List[A]) = xs.reverse
 	def isPalindrome[A] (xs: List[A]) = xs.equals(xs.reverse)
+	def flatten[A] (xs: List[A]): List[Any] = xs flatMap{
+	  case ys: List[_] => flatten(ys)
+	  case y => List(y)	  
+	}
+	//Note: could/should implement the following with dropWhile. Going to do the next problem with takeWhile
+	//TODO: come back and do this with dropWhile
+	def compress[A] (xs: List[A]) = {
+	  def compress0[A] (xs: List[A], acc: A): List[Any] = {
+	    if (xs.isEmpty) Nil
+	    else if (xs.head == acc) compress0(xs.tail, acc)
+	    else xs.head :: compress0(xs.tail, xs.head)
+	  }
+	  compress0(xs, Nil)
+	}
+	
+	//TODO: should use break/span instead of takeWhile/dropWhile
+	def pack[A](xs: List[A]): List[List[A]] = xs match {
+	  case Nil => Nil
+	  case ys => ys.takeWhile(_ == ys.head) :: pack(ys.dropWhile(_ == ys.head))
+	}
+	
+	def encode[A](xs: List[A]) = pack(xs).map(x => (x.length, x.head))
+	
+	def encodeModified[A](xs: List[A]) = pack(xs).map(x =>
+	  if (x.length == 1) x.head
+	  else (x.length, x.head))
+	
+	def	decode[A](xs: List[(Int,A)]) = xs flatMap{x => List.fill(x._1)(x._2)}
+	//P13 is a duplicate of a previous problem. TODO: go back later and do it  
+	
+	def duplicate[A](xs: List[A]) = xs flatMap{x => List(x,x)}
+	 
+	def duplicateN[A](i: Int, xs: List[A]) = xs flatMap{x => List.fill(i)(x)}
+	
+	//probably more efficient to do straight recursion with an accumulator. Could also zipWithIndex and then filter
+	def drop[A](i: Int, xs: List[A]): List[A] = xs match{
+	  case Nil => Nil
+	  case _ => xs.take(i - 1) ++ drop(i, xs.drop(i))
+	} 
+	
+	def split[A](i: Int, xs: List[A]): (List[A],List[A]) = xs.splitAt(i)
+	
+	def slice[A](i: Int, j: Int, xs: List[A]) = xs.slice(i,j)
+	
+	def rotate[A](i: Int, xs: List[A]) = {
+	  val rot = if (i > 0) i else xs.length + i 
+	  xs.drop(rot) ++ xs.take(rot) 
+	}
+	
+	//should use splitAt instead of take/drop
+	def removeAt[A](i: Int, xs: List[A]) = {
+	  val back = xs.drop(i)
+	  (xs.take(i) ++ back.tail, back.head)
+	}
+	
+	def insertAt[A](x: A, i: Int, xs:List[A]) = xs.splitAt(i) match{
+	  case (ys, zs) => ys ::: x :: zs 
+	}
+	
+	def range(i : Int, j : Int) = List.range(i,j+1)
+	
+	def randomSelect[A](i: Int, xs: List[A]) = {
+	  val r = new scala.util.Random
+	  def randomSelect0[A](i: Int, xs: List[A], r: util.Random): List[A] = {
+	    if (i <= 0) Nil
+	    else{
+	      val (rest,x) = removeAt(r.nextInt(xs.length), xs)
+	      x :: randomSelect0(i-1, rest, r)}
+	  }  
+	  randomSelect0(i, xs, r)
+	}
+	
+	//Note: The solution on http://aperiodic.net/phil/scala/s-99/ doesn't match the problem description.
+	def lotto(i: Int, j: Int) = randomSelect(i,List.range(1,j+1))
+	
+	def randomPermute[A](xs: List[A]) = randomSelect(xs.length, xs)
+	
+	//TODO: don't use builtin
+	def combinations[A](i: Int, xs: List[A]): List[List[A]]={
+	  xs.combinations(i).toList
+	}
 }
--- src/test/scala/ListsSuite.scala
+++ src/test/scala/ListsSuite.scala
@@ -40,4 +40,95 @@ class ListsSuite extends FunSuite {
    assert(isPalindrome(List(1, 2, 2, 1)) == true)
    assert(isPalindrome(List(1, 2, 1, 1)) == false)
   }    
+  
+  test("P07 -- recursive flatten"){
+    assert(flatten(List(List(1, 1), 2, List(3, List(5, 8)))) == List(1, 1, 2, 3, 5, 8))
+  }
+  
+  test("P08 -- compress"){
+    assert(compress(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) == List('a, 'b, 'c, 'a, 'd, 'e))
+  }
+  
+  test("P09 -- pack"){
+    assert(pack(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) == List(List('a, 'a, 'a, 'a), List('b), List('c, 'c), List('a, 'a), List('d), List('e, 'e, 'e, 'e)))
+  }  
+  
+  test("P10 -- encode"){
+	assert(encode(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) == List((4,'a), (1,'b), (2,'c), (2,'a), (1,'d), (4,'e)))
+  }
+  
+  test("P11 -- encodeModified"){
+	assert(encodeModified(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) == List((4,'a), 'b, (2,'c), (2,'a), 'd, (4,'e)))
+  }
+  
+  test("P12 -- decode"){
+    assert(decode(List((4, 'a), (1, 'b), (2, 'c), (2, 'a), (1, 'd), (4, 'e))) == List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+  }
+  
+  //P13 is a duplicate of a previous problem. TODO: go back later and do it
+  
+  test("P14 -- duplicate"){
+   assert(duplicate(List('a, 'b, 'c, 'c, 'd)) == List('a, 'a, 'b, 'b, 'c, 'c, 'c, 'c, 'd, 'd))
+  }
+  
+  test("P15 -- duplicateN"){
+    assert(duplicateN(3, List('a, 'b, 'c, 'c, 'd)) == List('a, 'a, 'a, 'b, 'b, 'b, 'c, 'c, 'c, 'c, 'c, 'c, 'd, 'd, 'd))
+  }
+  
+  test("P16 -- drop"){
+    assert(drop(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) == List('a, 'b, 'd, 'e, 'g, 'h, 'j, 'k))
+  }
+
+  test("P17 -- split"){
+     assert(split(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) == (List('a, 'b, 'c),List('d, 'e, 'f, 'g, 'h, 'i, 'j, 'k)))
+  }
+  
+  test("P18 -- slice"){
+    assert(slice(3, 7, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) == List('d, 'e, 'f, 'g))
+  }
+  
+  test("P19 -- rotate"){
+    assert(rotate(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) == List('d, 'e, 'f, 'g, 'h, 'i, 'j, 'k, 'a, 'b, 'c))
+    assert(rotate(-2, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) == List('j, 'k, 'a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i))
+  }
+  
+  test("P20 -- removeAt"){
+    assert(removeAt(1, List('a, 'b, 'c, 'd)) == (List('a, 'c, 'd),'b))
+  }
+
+  test("P21 -- insertAt"){
+    assert(insertAt('new, 1, List('a, 'b, 'c, 'd)) == List('a, 'new, 'b, 'c, 'd))
+  }
+  
+  test("P22 -- range"){
+     assert(range(4, 9) == List(4, 5, 6, 7, 8, 9))
+  }
+  
+  test("P23 -- randomSelect"){
+    //TODO: come up with a method of testing random selection
+    //Note that the current example on http://aperiodic.net/phil/scala/s-99/ is wrong
+  }
+  
+  test("P24 -- lotto"){
+    //TODO: come up with a method of testing random selection 
+  }
+  
+  test("P25 -- combinations"){
+    val c1 = combinations(1, List('a,'b,'c))
+    val a1 = List(List('a), List('b), List('c))
+    assert(c1.length == a1.length && c1.toSet == a1.toSet)
+    
+    val c2 = combinations(2, List('a,'b,'c))
+    val a2 = List(List('a, 'b), List('a, 'c), List('b, 'c))
+    assert(c2.length == a2.length && c2.toSet == a2.toSet)
+    
+    val c3 = combinations(3, List('a, 'b, 'c, 'd, 'e, 'f))
+    val a3 = List(List('a, 'b, 'c), List('a, 'b, 'd), List('a,
+                                                  'b, 'e), List('a, 'b, 'f), List('a, 'c, 'd), List('a, 'c, 'e), List('a, 'c,
+                                                  'f), List('a, 'd, 'e), List('a, 'd, 'f), List('a, 'e, 'f), List('b, 'c, 'd)
+                                                  , List('b, 'c, 'e), List('b, 'c, 'f), List('b, 'd, 'e), List('b, 'd, 'f), List
+                                                   ('b, 'e, 'f), List('c, 'd, 'e), List('c, 'd, 'f), List('c, 'e, 'f), List(
+                                                   'd, 'e, 'f))
+    assert(c3.length == a3.length && c3.toSet == a3.toSet)                                                                                               
+  }
 }
