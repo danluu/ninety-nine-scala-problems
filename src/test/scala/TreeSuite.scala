85c5d5a52321a3316165db22ab78d27150bb724b
Dan Luu <danluu@gmail.com>
2012-11-12 13:01:13 -0600

    setup for binary tree problems

---
+++ src/test/scala/TreeSuite.scala
@@ -0,0 +1,11 @@
+package ninetynine
+
+import org.scalatest.FunSuite
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class TreeSuite extends FunSuite {
+  
+}
