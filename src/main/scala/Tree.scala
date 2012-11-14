f0b3ce2cabb489d62123585f91ceaed8f9d19c02
Dan Luu <danluu@gmail.com>
2012-11-14 00:11:00 -0600

    up to 58, symmetric balanced binary trees

--- src/main/scala/Tree.scala
+++ src/main/scala/Tree.scala
@@ -1,16 +1,64 @@
 package ninetynine {
 
-  sealed abstract class Tree[+T]
+  sealed abstract class Tree[+T]{
+    def isMirrorOf[U](t: Tree[U]): Boolean
+    def isSymmetric: Boolean
+    def addValue[U >: T <% Ordered[U]](x: U): Tree[U]
+  }
 
   case class Node[+T](value: T, left: Tree[T], right: Tree[T]) extends Tree[T] {
-    override def toString = "T(" + value.toString + " " + left.toString + " " + right.toString + ")"
+    def leaves = "," + left.toString + "," + right.toString
+    def tailString = if (leaves == ",End,End") "" 
+    else leaves
+    override def toString = "Node(" + value.toString + tailString + ")"
+    override def isMirrorOf[U](t: Tree[U]) = {
+      def isMirrorOf0[U](t0: Tree[U]) = t0 match {
+        case n0: Node[U] => left.isMirrorOf(n0.left) && right.isMirrorOf(n0.right)  
+        case _ => false
+      }
+      t match {
+        case n: Node[U] => left.isMirrorOf(n.right) && right.isMirrorOf(n.left)
+        case _ => false
+      }
+    }
+    override def isSymmetric = left.isMirrorOf(right) 
+    //TODO: think about isSymmetric and write more tests. Something feels off
+    
+    override def addValue[U >: T <% Ordered[U]](x: U) = 
+      if (x < value) Node(value,left.addValue(x),right)
+      else Node(value, left, right.addValue(x))
   }
 
   case object End extends Tree[Nothing] {
-    override def toString = "."
+    override def toString = "End"
+    override def isMirrorOf[U](t: Tree[U]) = t == End
+    override def isSymmetric = true
+    override def addValue[U >: Nothing <% Ordered[U]](x: U) = Node(x)
   }
 
   object Node {
     def apply[T](value: T): Node[T] = Node(value, End, End)
   }
+  
+  object Tree{   
+	def cBalanced[T](i: Int, x: T): List[Tree[T]] = {
+	  if (i <= 0) List(End)
+	  // else if (i % 2 == 1) Node(x,cBalanced(i/2,x)),cBalanced(i/2,x)) want to write this, but with flatMap
+	  // because cBalanced returns a list and not a Node. Alternately, could use a for comp
+	  else if (i % 2 == 1){ 
+	    val t = cBalanced(i/2, x)
+	    t.flatMap(l => t.map(r => Node(x,l,r)))
+	  }else{ // i % 2 == 0
+	    val t1 = cBalanced(i/2,x)
+	    val t2 = cBalanced(i/2-1,x)
+	    t1.flatMap(big => t2.flatMap(small => List(Node(x, big, small), Node(x, small, big))))
+	  }
+	}
+	
+	def fromList[T <% Ordered[T]](l: List[T]): Tree[T] = l.foldLeft(End: Tree[T])((t,x) => t.addValue(x))
+	
+	def symmetricBalancedTrees[T](i: Int, x: T): List[Tree[T]] = cBalanced(i,x).filter(_.isSymmetric)
+	
+  }
+  
 }
--- src/test/scala/TreeSuite.scala
+++ src/test/scala/TreeSuite.scala
@@ -7,5 +7,52 @@ import org.scalatest.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
 class TreeSuite extends FunSuite {
+  test("P55 - completely balanced binary trees"){
+    assert(Tree.cBalanced(1,'a) == List(Node('a)))
+    assert(Tree.cBalanced(2,'b) == List(Node('b,Node('b),End), Node('b,End,Node('b))))
+    assert(Tree.cBalanced(3,'c) == List(Node('c,Node('c),Node('c))))
+    
+  }
+  
+  test("P66 - isSymmetric"){
+    assert(Node('a', Node('b'), Node('c')).isSymmetric == true)
+    assert(Node('a', End, Node('c')).isSymmetric == false)
+    assert(Node('a', Node('b'), End).isSymmetric == false)
+    assert(Node('a').isSymmetric == true)
+    
+    val rgt = Node('a,End,Node('b,End,Node('c)))
+    val lft = Node('a,Node('b,Node('c),End),End)
+    assert(Node('x,lft,rgt).isSymmetric == true)
+    assert(Node('x,rgt,lft).isSymmetric == true)
+    assert(Node('x,rgt,rgt).isSymmetric == false)
+    assert(Node('x,lft,lft).isSymmetric == false)
+  }
+  
+  test("P67 - (unbalanced) binary search tree"){
+    assert(End.addValue(2).addValue(3).addValue(0) == Node(2,Node(0),Node(3)))
+    assert(Tree.fromList(List(3, 2, 5, 7, 1)) == Node(3,Node(2,Node(1),End),Node(5,End,Node(7))))
+  }
+  
+  test("P66 + P67"){
+    assert(Tree.fromList(List(5, 3, 18, 1, 4, 12, 21)).isSymmetric == true)
+    assert(Tree.fromList(List(3, 2, 5, 7, 4)).isSymmetric == false)
+  }
+  
+  test("P68 - symmetric completely balanced binary trees"){
+    val l = Tree.symmetricBalancedTrees(5, 'x)
+    assert(l.length == 2)
+    assert(l.toSet == Set(Node('x,Node('x,Node('x),End),Node('x,End,Node('x))), Node('x,Node('x,End,Node('x)),Node('x,Node('x),End))))
+    
+    val evenSymmetricTrees = for{
+      i <- (1 to 10) //for any even number except 0, there should be no possible tree. 'End', by itself, works for 0
+      if i % 2 == 0
+      } yield Tree.symmetricBalancedTrees(i, 'x)
+      
+     assert(evenSymmetricTrees.toList.flatten.length == 0)
+     //for the odd numbers, the pattern sequence should be: 1, 1, 2, 1, 4, 4, 4, 1, 8, 16, 32, 16, 32, 16, 8, 1, 16, 64, 256, 256, 1024
+     // i.e., a(2n) = 2a(n)a(n-1), a(2n+1) = a(n)a(n). Generating all cBalanced trees with our method makes this too slow to
+     // test for large numbers, though
+  }
+  
   
 }
